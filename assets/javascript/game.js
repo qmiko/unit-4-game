dae4c956551c992581ede0565a11b7c7736917ab
shaquima <senoritabroccolini@gmail.com>
2019-04-29 21:55:54 -0500

    starting-js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var targetNumber = [];
+var counter = 0;
+var numberOptions = [4, 3, 9, 11];
+
+for (var i = 0; i < numberOptions.length; i++){
+    var imageCrystal = $("<img>");
+
+    imageCrystal.addClass("crystal-image");
+
+    imageCrystal.attr("src", "https://www.shutterstock.com/image-photo/violet-crystal-stone-macro-mineral-purple-1320122969")
+}
