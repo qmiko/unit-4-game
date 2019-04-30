9aa678df9781e02de615318ed05837e27df338bf
shaquima <senoritabroccolini@gmail.com>
2019-04-29 22:06:23 -0500

    added js to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,5 +7,10 @@ for (var i = 0; i < numberOptions.length; i++){
 
     imageCrystal.addClass("crystal-image");
 
-    imageCrystal.attr("src", "https://www.shutterstock.com/image-photo/violet-crystal-stone-macro-mineral-purple-1320122969")
+    imageCrystal.attr("src", "https://www.shutterstock.com/image-photo/violet-crystal-stone-macro-mineral-purple-1320122969");
+
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    $("#crystals").append(imageCrystal);
+
 }
